0ffd0386c1aa7e4a03f1135b03ad9e8577fcfb06
yoshiki <y.nagatsuka0524@gmail.com>
2020-11-01 00:38:39 +0900

    capistrano-puma追加

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "104.196.248.104", user: "yoshiki", roles: %w{app}
+server ENV["SERVER_IP"], user: "yoshiki", roles: %w{app}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
