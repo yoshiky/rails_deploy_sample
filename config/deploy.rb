7ec796c70fc9f1eadddaf961fdaf8cd5ea69a136
yoshiki <y.nagatsuka0524@gmail.com>
2020-11-02 08:14:07 +0900

    bundle_path変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :branch, ENV["BRANCH"] || 'main'
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/yoshiki/app/#{fetch(:application)}"
 
-set :bundle_path, './vendor/bundle'
+set :bundle_path, -> { shared_path.join('bundle') }
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
